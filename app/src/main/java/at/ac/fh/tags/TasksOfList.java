f6a0ec54f3cfef7345da4797103db7489efd375a
eh-bianca <bianca.ehrenstrasser@outlook.com>
2015-02-07 15:27:19 +0100

    neuer Task Button in TasksOfList

--- app/src/main/java/at/ac/fh/tags/TasksOfList.java
+++ app/src/main/java/at/ac/fh/tags/TasksOfList.java
@@ -145,16 +145,16 @@ public class TasksOfList extends Activity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        if (id == R.id.action_new) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+            //Intents wurden geändert, damit die aufgerufene Activity weiß, wovon sie aufgerufen wurde
+            Intent intent = new Intent(this, ManageTaskActivity.class);
+            intent.putExtra("Source", 0);
+            startActivity(intent);
+
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
